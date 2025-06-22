b39ba88366085fbc70856cbef33977b17313219b
ДимК <dimaszone@hotmail.com>
2025-06-22 20:16:07 +0300

    add docker

--- conftest.py
+++ conftest.py
@@ -1,13 +1,24 @@
+import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HEADLESS = os.getenv("HEADLESS", "false").lower() == "true"
+
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
     options = Options()
-    #options.add_argument("--headless")
+    if HEADLESS:
+        # New, native Headless mode
+        options.add_argument("--headless=new")
+        # Often used in Lambda, Cloud Functions scenarios and Docker
+        options.add_argument("--disable-dev-shm-usage")
+
     options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
